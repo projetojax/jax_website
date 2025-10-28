a71870a58bf9319c2c8ec609448f2af91bcb2c68
ProjetoJAX <projetojaxoficial@gmail.com>
2025-10-28 16:46:51 +0000

    att modelos

--- app/models.py
+++ app/models.py
@@ -18,8 +18,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     
-    id = db.Column(db.Integer, pri
-    ry_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
